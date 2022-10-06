4051857e58b810e60b5732f42b4ae69e57d651bd
Vanhung1004 <111971264+Vanhung1004@users.noreply.github.com>
2022-10-06 21:05:57 +0700

    Add files via upload

---
+++ vanhung/js/moi.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["HERO", "VĂN HÙNG", "DESIGNER", "WEB DESIGNER", "PHOTOGRAPHER"],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
